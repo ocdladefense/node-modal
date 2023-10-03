468b0bd513983ff397171af6b1c3159dd0bd17a5
Ubuntu <ubuntu@ip-172-31-58-171.us-west-2.compute.internal>
2023-10-02 20:58:15 -0700

    Updated modal files.

--- dist/modal.js
+++ dist/modal.js
@@ -8,6 +8,17 @@ modal.show();
 import { vNode, View } from "../../view/view.js";
 export { Modal, ModalComponent };
 const Modal = function () {
+  let domAttached = false;
+  let defaultLeftNavStyles = "display:inline-block;width:25%; vertical-align:top;overflow-y:auto;overflow-y: auto;position: sticky;max-height: 600px;padding-right:25px;";
+  defaultLeftNavStyles = "display:none; width:25%; vertical-align:top;overflow-y:auto;overflow-y: auto;position: sticky;max-height: 600px;padding-right:25px;";
+  let defaultStyles = "display:inline-block; width:74%; vertical-align:top; overflow-y: auto; overflow-y: auto; padding: 35px; calc(100% + 100px);";
+
+  // let leftNavStyles = !config.leftNav ? (defaultLeftNavStyles + "display:none;") : (defaultLeftNavStyles + "display:inline-block;");
+
+  let defaultConfig = {
+    defaultLeftNavStyles,
+    defaultStyles
+  };
   let proto = {
     show: function () {
       document.body.classList.add("has-modal");
@@ -18,17 +29,34 @@ const Modal = function () {
       //document.getElementById("modal").classList.remove("fullscreen")
       setTimeout(() => document.body.classList.remove('has-modal'), 100);
     },
+    attach: function () {
+      if (domAttached) return;
+      let vnode = vNode(ModalComponent, {
+        content: "",
+        config: this.config
+      });
+      let node = View.createElement(vnode);
+      document.body.appendChild(node);
+      document.addEventListener("click", this);
+      domAttached = true;
+    },
     render: function (html) {
+      if (!domAttached) {
+        this.attach();
+      }
       //if modal exists
       let modalElement = document.getElementById("modal");
       if (modalElement != null) {
         modalElement.remove();
       }
       let vnode = vNode(ModalComponent, {
-        content: html
+        content: "",
+        config: this.config
       });
       let node = View.createElement(vnode);
-      document.body.appendChild(node);
+      document.body.replaceChild(node, document.getElementById("modal-backdrop"));
+      document.getElementById("modal-content").innerHTML = html;
+      document.body.classList.remove("loading");
     },
     renderHtml: function (html, targetId) {
       document.body.classList.remove("loading");
@@ -60,17 +88,18 @@ const Modal = function () {
       this.hide();
     }
   };
-  function Modal() {
-    this.body = document.body;
-    //this.modal = document.getElementById("modal");
-
-    document.addEventListener("click", this);
+  function Modal(config) {
+    this.visible = false;
+    this.config = config || defaultConfig;
   }
   Modal.prototype = proto;
   return Modal;
 }();
 const ModalComponent = function (props) {
   let content = props.content;
+  let config = props.config;
+  let customStyles = config.style;
+  let leftNavStyles = config.defaultLeftNavStyles;
   return vNode("div", {
     id: "modal-backdrop"
   }, vNode("div", {
@@ -82,9 +111,10 @@ const ModalComponent = function (props) {
     id: "modal-body",
     style: "vertical-align:top;"
   }, vNode("div", {
-    id: "modal-title-bar"
+    id: "modal-title-bar",
+    style: "text-align:right;"
   }, vNode("button", {
-    style: "float:right;",
+    style: "float:none;",
     id: "close-modal",
     type: "button"
   }, "X"), vNode("div", {
@@ -92,10 +122,10 @@ const ModalComponent = function (props) {
   })), vNode("div", {
     id: "modal-left-nav",
     class: "modal-toc",
-    style: "display:inline-block;width:25%; vertical-align:top;overflow-y:auto;overflow-y: auto;position: sticky;max-height: 600px;padding-right:25px;"
+    style: leftNavStyles
   }), vNode("div", {
     id: "modal-content",
-    style: "display:inline-block; width:74%; vertical-align:top; overflow-y: auto; overflow-y: auto; max-height: 600px; padding: 35px;"
+    style: customStyles || defaultStyles
   }, content))), vNode("div", {
     id: "loading"
   }, vNode("div", {
--- src/modal.js
+++ src/modal.js
@@ -10,9 +10,27 @@ export { Modal, ModalComponent };
 
 
 const Modal = (function() {
+
+    let domAttached = false;
+
+    let defaultLeftNavStyles = "display:inline-block;width:25%; vertical-align:top;overflow-y:auto;overflow-y: auto;position: sticky;max-height: 600px;padding-right:25px;";
+
+    defaultLeftNavStyles = "display:none; width:25%; vertical-align:top;overflow-y:auto;overflow-y: auto;position: sticky;max-height: 600px;padding-right:25px;";
+
+    let defaultStyles = "display:inline-block; width:74%; vertical-align:top; overflow-y: auto; overflow-y: auto; padding: 35px; calc(100% + 100px);";
+
+    // let leftNavStyles = !config.leftNav ? (defaultLeftNavStyles + "display:none;") : (defaultLeftNavStyles + "display:inline-block;");
+
+    let defaultConfig = {
+        defaultLeftNavStyles,
+        defaultStyles
+    };
+
+
+
     let proto = {
 
-        show: function(){
+        show: function() {
             document.body.classList.add("has-modal");
             document.body.classList.add("loading");
             setTimeout(() => document.getElementById("modal").classList.add("fullscreen"), 100);
@@ -24,16 +42,29 @@ const Modal = (function() {
             setTimeout(() => document.body.classList.remove('has-modal'), 100);
         },
         
+        attach: function() {
+            if(domAttached) return;
+            let vnode = <ModalComponent content="" config={this.config} />;
+            let node = View.createElement(vnode);
+            document.body.appendChild(node);
+            document.addEventListener("click", this);
+            domAttached = true;
+        },
         
-        render: function(html){
+        render: function(html) {
+            if(!domAttached) {
+                this.attach();
+            }
             //if modal exists
             let modalElement = document.getElementById("modal");
             if(modalElement != null){
                 modalElement.remove();
             }
-            let vnode =  <ModalComponent content={html} />;
+            let vnode =  <ModalComponent content="" config={this.config} />;
             let node = View.createElement(vnode);
-            document.body.appendChild(node);
+            document.body.replaceChild(node, document.getElementById("modal-backdrop"));
+            document.getElementById("modal-content").innerHTML = html;
+            document.body.classList.remove("loading");
         },
 
         
@@ -70,16 +101,14 @@ const Modal = (function() {
 
             e.preventDefault();
             this.hide();
-            
         }
     };
 
 
-    function Modal() {
-        this.body = document.body;
-        //this.modal = document.getElementById("modal");
+    function Modal(config) {
+        this.visible = false;
+        this.config = config || defaultConfig;
         
-        document.addEventListener("click", this);
     }
 
     Modal.prototype = proto;
@@ -87,21 +116,29 @@ const Modal = (function() {
     return Modal;
 })();
 
+
+
 const ModalComponent = function(props){
     let content = props.content;
+    let config = props.config;
+    let customStyles = config.style;
+    let leftNavStyles = config.defaultLeftNavStyles;
+
+
+
     return (
     <div id="modal-backdrop">
             <div id="modal">
                 <div id="modal-container" style="overflow-y:visible;"> 
                     <div id="modal-body" style="vertical-align:top;">
-                        <div id="modal-title-bar">
-                            <button style="float:right;" id="close-modal" type="button">X</button>
+                        <div id="modal-title-bar" style="text-align:right;">
+                            <button style="float:none;" id="close-modal" type="button">X</button>
                             <div id="modal-title-bar-title"></div>
                         </div>
-                        <div id="modal-left-nav" class="modal-toc" style="display:inline-block;width:25%; vertical-align:top;overflow-y:auto;overflow-y: auto;position: sticky;max-height: 600px;padding-right:25px;">
+                        <div id="modal-left-nav" class="modal-toc" style={leftNavStyles}>
 
                         </div>
-                        <div id="modal-content" style="display:inline-block; width:74%; vertical-align:top; overflow-y: auto; overflow-y: auto; max-height: 600px; padding: 35px;">
+                        <div id="modal-content" style={customStyles || defaultStyles}>
                         {content}
                         </div>
                     </div>
